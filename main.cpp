fba752d4ad3ae3c17a9589f2c47dfb9e2386bba7
P Paul Jonathan <ppauljonathan@gmail.com>
2022-05-19 21:37:35 +0530

    made a socket to get wifi essid

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
-#include<unistd.h>
-#include<fstream>
-#include<string>
-#include<vector>
-#include<ctime>
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h> 
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/wireless.h>
+#include <alsa/asoundlib.h>
+
 using namespace std;
 
-string version="v1.0.2",authour="P Paul Jonathan";
+string version="v2.0.1",authour="P Paul Jonathan";
 
 vector <string> split_string(string s,char delim){
     auto j=s.cbegin();
@@ -92,20 +104,28 @@ void dt(){
 }
 
 void ssid(){
-    FILE *net=popen("iw wlo1 info | cut -d '\n' -f 5","r");
-    char c;
+    struct iwreq wreq;
     string ssid_data;
-    while((c=fgetc(net))!='\n'){
-        if(c!='\t')ssid_data.push_back(c);
-    }
+    int sockfd;
+    char * id;
 
-    do{
-        ssid_data.erase(ssid_data.begin());
-    }while(ssid_data[0]!=' ');
-    if(ssid_data==" managed"){cout<<" NO INTERNET";}
-    else{cout<<" NET"<<ssid_data;}
+    memset(&wreq, 0, sizeof(struct iwreq));
+    sprintf(wreq.ifr_name, "wlo1");
+
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        cout<<" INTERNET ERROR";
+    }
+    char buffer[32];
+    memset(buffer, 0, 32);
+    wreq.u.essid.pointer = buffer;
+    wreq.u.essid.length = 32;
+    if (ioctl(sockfd,SIOCGIWESSID, &wreq) == -1) {
+        cout<<" INTERNET ERROR";
+    }
+    wreq.u.essid.length;
+    if(wreq.u.essid.length == 0){cout<<" NO INTERNET";}
+    else{printf(" NET %s",wreq.u.essid.pointer);}
     cout<<" |";
-    fclose(net);
 }
 
 void help(){
